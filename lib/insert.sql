9336e782cd91ff63a2ec6a87b2e7eae9ab74e854
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-27 18:59:25 +0300

    Organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES 
+  (1, "Mr. Chocolate", 15, "M", "Dark Brown", "Calm", 1),
+  (2, "Rowdy", 13, "M", "Brown", "Calm", 0),
+  (3, "Tabitha", 16, "F", "White", "Outgoing", 0),
+  (4, "Sergeant Brown", 10, "M", "Brown", "Hostile", 1),
+  (5, "Melissa", 8, "F", "White", "Calm", 1),
+  (6, "Grinch", 4, "M", "Black", "Petty", 0),
+  (7, "Wendy", 3, "F", "Light Brown", "happy", 1),
+  (8, NULL, 20, "M", "White Brown", "Playful", 1)
+;
